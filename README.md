21b467273a09bd156ca6eb74933f160c4810363e
Ramon Audain <raudain123@gmail.com>
2019-12-21 18:59:08 -0800

    migration table created

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Backend
+
+Project Name
+African Marketplace 1
+
+Pitch
+Sauti Africa empowers small business owners, particularly women, to improve their business and economic opportunities to grow out of poverty.  You will build a platform to enable these business owners to create listing for items they have for sale. 
+
+Example product reference list available here: https://docs.google.com/spreadsheets/d/1cbJQL1JbO5pAV4mss1yH0uMp58lF8TkKZt6NPkF9NLg/edit#gid=0
+
+Organization link: http://sautiafrica.org/
+
+MVP
+- A small business owner can log on and see relevant prices in various categories to help them set their own prices. 
+- They can also make listing for what they want to sell, which will show up to all users.  
+- They add a new item by selecting their market location, and then typing in their item name, description, and price. 
+
+Stretch
+- Business owners can upload their picture to their profile
--- data/migrations/20191221164629_user-tables.js
+++ data/migrations/20191221164629_user-tables.js
@@ -1,8 +1,45 @@
 
 exports.up = function(knex) {
-    return
+    return knex.schema
+        .createTable("Users", table => {
+            table.increments();
+
+            table.string("username", 255)
+                .notNullable()
+                .unique();
+            
+            table.string("password", 255)
+                .notNullable()
+            
+            table.string("email", 255)
+                .notNullable()
+                .unique();
+
+            table.string("")
+        })
+        .createTable("Items", table => {
+            table.increments();
+
+            table.string("item_name", 255).notNullable()
+            
+            table.string("item_description", 255).notNullable()
+            
+            table.float("item_price").notNullable()
+
+            table.string("market_location", 255).notNullable()
+
+            table.integer("uid")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("Users")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+        });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists("Items")
+        .dropTableIfExists("Users")
 };
