edc2a3e13974a8f0bb5125941f74b7bbd69f3c49
Ramon Audain <raudain123@gmail.com>
2019-12-21 20:38:20 -0800

    token authentcation middleware created

---
+++ auth/middleware/authenticate.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    const {authorization} = req.headers;
+
+    if(authorization){
+        const secret = process.env.JWT_SECRET || "stay secret";
+
+        jwt.verify(authorization, secret, function(err, decodedToken){
+            if(err){
+                res.status(401).json({message: "Invalid token."});
+            }
+            else{
+                req.token = decodedToken;
+                next();
+            }
+        });
+    }
+    else{
+        res.status(401).json({message: "Token needed."})
+    }
+};
