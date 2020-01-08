f3cadf807b97deb6ad92471f361a4e84a0544d3a
Ramon Audain <raudain123@gmail.com>
2020-01-08 14:37:56 -0800

    Update items-router.js

--- data/helpers/items/items-router.js
+++ data/helpers/items/items-router.js
@@ -33,8 +33,8 @@ router.get("/:id/items/:id",
 (req, res) => {
     const {id} = req.params;
 
-    Items
-        .getItems(id)
+    User
+        .getUser(id)
         .then(() => {
             return Items
                 .getItem(id)
